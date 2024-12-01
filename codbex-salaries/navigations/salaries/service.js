36e8e053fc7284190423c46a1ba33527ff72dfdb
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-12-01 16:15:15 +0200

    add salaries navigation (#9)

---
+++ codbex-salaries/navigations/salaries/service.js
@@ -0,0 +1,19 @@
+const navigationData = {
+    id: 'salaries-navigation',
+    label: "Salaries",
+    group: "salaries",
+    order: 1000,
+    link: "/services/web/codbex-salaries/gen/codbex-salaries/ui/Salaries/index.html?embedded"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
+
+
